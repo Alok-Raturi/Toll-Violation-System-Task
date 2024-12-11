d32001e2819a0c22322919f72e5738ed0e04a155
Alok Raturi <alok.raturi1508@gmail.com>
2024-12-11 13:27:47 +0530

    Add Private access of storage account

--- Terraform IAC/backend.tf
+++ Terraform IAC/backend.tf
@@ -3,7 +3,8 @@ terraform {
     resource_group_name  = "terraform-backend"
     storage_account_name = "terraformstoragealok"                      
     container_name       = "terraform-state-data"                       
-    key                  = ""        
+    access_key = "EOWs2jE5Gp20i2F+S4OpfrtxnAbK+RBM0zr1/ti7pkiMAzK9mXX737X1SC72ehzQmzaX73C4by+p+ASth5Wxsg=="
+    key = "terraform.tfstate"        
   }
 }
 
--- Terraform IAC/main.tf
+++ Terraform IAC/main.tf
@@ -9,5 +9,10 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = ""
+  subscription_id = "8a228cff-023f-42af-818d-51a84a828d46"
+}
+
+resource "azurerm_resource_group" "terraform_backend" {
+  name     = "terraform-backend"
+  location = "Central India"
 }
