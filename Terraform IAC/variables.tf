882de462802bda299d9eb5f42162072648e886e8
J007acky <rahulrautela21@gmail.com>
2025-01-06 10:23:06 +0530

    Updated code for New Azure Account

--- Terraform IAC/variables.tf
+++ Terraform IAC/variables.tf
@@ -1,22 +1,18 @@
 variable "subscription_id" {
   description = "The subscription ID in which the resources will be created."
-  default = "8a228cff-023f-42af-818d-51a84a828d46"
   type = string
 }
 
 variable "tenant_id" {
   description = "The Tenant ID for the target Azure Account"
-#  default = "46118836-cde0-431b-bb1e-b6ab7887003c"
   type = string
 }
 variable "client_id" {
   description = "Client ID for the Application"
-#  default= "20c516e5-a70b-4ebc-bbed-4f2dd86c3141"
   type = string
 }
 variable "client_secret" {
   description = "Client Secret"
-#  default="bH18Q~9cOEq7OH1m3jxXHSOwg_PjSYn38IM9Saik"
   type = string
 }
 
